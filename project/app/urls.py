cb428bf602db210debef7e355e4dd94ee429501c
adith <adithkkadith@gmail.com>
2025-02-17 21:42:45 +0530

    home page updates 17/2/24

--- project/app/urls.py
+++ project/app/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('',  views.home, name='home'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('about/',views.about, name='about'),
       
 ]
 
