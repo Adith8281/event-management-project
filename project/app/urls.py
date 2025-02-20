5b5decc171b9b17fceae1c2af1cecd904259cd88
adith <adithkkadith@gmail.com>
2025-02-20 19:23:24 +0530

    update team

--- project/app/urls.py
+++ project/app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('',  views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/',views.about, name='about'),
+    path('team/',views.team,name='team'),
       
 ]
 
